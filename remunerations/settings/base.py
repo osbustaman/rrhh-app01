85dac41e83ba8734c3b30449a7fab54798fa6003
dmentron <oabn.trabajo@gmail.com>
2024-04-30 00:22:24 -0400

    upload

--- remunerations/settings/base.py
+++ remunerations/settings/base.py
@@ -26,9 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-	"http://127.0.0.1:8000"
+    "http://localhost:5173",
 ]
 
 CORS_ALLOW_METHODS = (
@@ -56,7 +56,8 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     'rest_framework',
-    'drf_yasg'
+    'drf_yasg',
+    'corsheaders'
 ]
 
 LOCAL_APPS = [
@@ -69,6 +70,7 @@ LOCAL_APPS = [
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'remunerations.middleware.multi_db.multidb_middleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
